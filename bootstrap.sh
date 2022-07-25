674e5b91cf86c85e6f79e7fb68f7a8d172d4e657
leopaglia <paglialungalf@gmail.com>
2022-07-25 03:33:29 -0300

    fix export

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,7 +53,7 @@ brew cleanup
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-export ZSH_CUSTOM = "$HOME/.ohmyzsh/custom"
+export ZSH_CUSTOM="$HOME/.ohmyzsh/custom"
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
